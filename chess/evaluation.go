0c841a2dec6cc8db591e003788dd8ab2402d9f4b
Kenrick Rilee <kenrick.rilee@gmail.com>
2023-02-06 22:43:29 -0800

    more reorg

---
+++ chess/evaluation.go
@@ -0,0 +1,139 @@
+package chess
+
+type EvaluationBitboard struct {
+	multiplier int
+	b          Bitboard
+}
+
+var DEVELOPMENT_SCALE = 50
+
+var ROOK_DEVELOPMENT_BITBOARDS = evaluationsPerPlayer([8][8]int{
+	{0, 0, 0, 0, 0, 0, 0, 0},
+	{1, 2, 2, 2, 2, 2, 2, 1},
+	{-1, 0, 0, 0, 0, 0, 0, -1},
+	{-1, 0, 0, 0, 0, 0, 0, -1},
+	{-1, 0, 0, 0, 0, 0, 0, -1},
+	{-1, 0, 0, 0, 0, 0, 0, -1},
+	{-1, 0, 0, 0, 0, 0, 0, -1},
+	{0, 0, 0, 2, 2, 0, 0, 0},
+}, DEVELOPMENT_SCALE)
+
+var PAWN_DEVELOPMENT_BITBOARDS = evaluationsPerPlayer([8][8]int{
+	{0, 0, 0, 0, 0, 0, 0, 0},
+	{3, 3, 3, 4, 4, 3, 3, 3},
+	{2, 2, 2, 3, 3, 2, 2, 2},
+	{2, 2, 2, 3, 3, 2, 2, 2},
+	{1, 1, 1, 3, 3, 1, 1, 1},
+	{0, 0, 0, 2, 2, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0},
+}, DEVELOPMENT_SCALE)
+
+var BISHOP_DEVELOPMENT_BITBOARDS = evaluationsPerPlayer([8][8]int{
+	{-1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, 0, 0, 0, 0, 0, 0, -1},
+	{-1, 0, 1, 2, 2, 1, 0, -1},
+	{-1, 1, 1, 2, 2, 1, 1, -1},
+	{-1, 0, 2, 2, 2, 2, 0, -1},
+	{-1, 2, 2, 2, 2, 2, 2, -1},
+	{-1, 1, 0, 0, 0, 0, 1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1},
+}, DEVELOPMENT_SCALE)
+var KNIGHT_DEVELOPMENT_BITBOARDS = evaluationsPerPlayer([8][8]int{
+	{-2, -2, -2, -2, -2, -2, -2, -2},
+	{-2, -1, 0, 0, 0, 0, -1, -2},
+	{-2, 0, 1, 2, 2, 1, 0, -2},
+	{-2, 1, 2, 2, 2, 2, 1, -2},
+	{-2, 0, 2, 2, 2, 2, 0, -2},
+	{-2, 1, 1, 2, 2, 1, 1, -2},
+	{-2, -1, 0, 0, 0, 0, -1, -2},
+	{-2, -2, -2, -2, -2, -2, -2, -2},
+}, DEVELOPMENT_SCALE)
+var QUEEN_DEVELOPMENT_BITBOARDS = evaluationsPerPlayer([8][8]int{
+	{-2, -2, -2, -1, -1, -2, -2, -2},
+	{-2, 0, 0, 0, 0, 0, 0, -2},
+	{-2, 0, 1, 1, 1, 1, 0, -2},
+	{-1, 0, 1, 1, 1, 1, 0, -1},
+	{0, 0, 1, 1, 1, 1, 0, 0},
+	{-2, 0, 1, 1, 1, 1, 0, -2},
+	{-2, 0, 1, 0, 0, 1, 0, -2},
+	{-2, -2, -2, -1, -1, -2, -2, -20},
+}, DEVELOPMENT_SCALE)
+
+func bitboardFromArray(lookup int, array [8][8]int) Bitboard {
+	b := Bitboard(0)
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			if array[i][j] == lookup {
+				index := (7-i)*8 + j
+				b |= singleBitboard(index)
+			}
+		}
+	}
+	return b
+}
+
+func evaluationsFromArray(array [8][8]int, scale int) []EvaluationBitboard {
+	result := []EvaluationBitboard{}
+	scores := map[int]bool{}
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			scores[array[i][j]] = true
+		}
+	}
+	for k := range scores {
+		eval := EvaluationBitboard{}
+		eval.multiplier = k * scale
+		eval.b = bitboardFromArray(k, array)
+		result = append(result, eval)
+	}
+	return result
+}
+
+func evaluationsPerPlayer(whiteOrientedEvalArray [8][8]int, scale int) [2][]EvaluationBitboard {
+	return [2][]EvaluationBitboard{
+		evaluationsFromArray(whiteOrientedEvalArray, scale),
+		evaluationsFromArray(flipArray(whiteOrientedEvalArray), scale),
+	}
+}
+
+func evaluateDevelopment(b Bitboard, e []EvaluationBitboard) int {
+	result := 0
+	for _, eval := range e {
+		result += eval.multiplier * OnesCount(eval.b&b)
+	}
+	return result
+}
+
+func (b *Bitboards) evaluateDevelopment(player Player) int {
+	development := 0
+	development += evaluateDevelopment(b.players[player].pieces[ROOK], ROOK_DEVELOPMENT_BITBOARDS[player])
+	development += evaluateDevelopment(b.players[player].pieces[KNIGHT], KNIGHT_DEVELOPMENT_BITBOARDS[player])
+	development += evaluateDevelopment(b.players[player].pieces[BISHOP], BISHOP_DEVELOPMENT_BITBOARDS[player])
+	development += evaluateDevelopment(b.players[player].pieces[QUEEN], QUEEN_DEVELOPMENT_BITBOARDS[player])
+	development += evaluateDevelopment(b.players[player].pieces[PAWN], PAWN_DEVELOPMENT_BITBOARDS[player])
+	return development
+}
+
+func (b *Bitboards) evaluate(player Player) int {
+	enemy := player.other()
+
+	pieceValues :=
+		500*OnesCount(b.players[player].pieces[ROOK]) +
+			300*OnesCount(b.players[player].pieces[KNIGHT]) +
+			350*OnesCount(b.players[player].pieces[BISHOP]) +
+			900*OnesCount(b.players[player].pieces[QUEEN]) +
+			100*OnesCount(b.players[player].pieces[PAWN])
+
+	enemyValues :=
+		500*OnesCount(b.players[enemy].pieces[ROOK]) +
+			300*OnesCount(b.players[enemy].pieces[KNIGHT]) +
+			350*OnesCount(b.players[enemy].pieces[BISHOP]) +
+			900*OnesCount(b.players[enemy].pieces[QUEEN]) +
+			100*OnesCount(b.players[enemy].pieces[PAWN])
+
+	developmentValues := b.evaluateDevelopment(player)
+	enemyDevelopmentValues := b.evaluateDevelopment(enemy)
+
+	return pieceValues + developmentValues - enemyValues - enemyDevelopmentValues
+}
--- chess/unit_test.go
+++ chess/unit_test.go
@@ -15,6 +15,141 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestBoardPrint(t *testing.T) {
+	b := BoardArray{
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+	}
+
+	assert.Equal(t, b.String(), strings.Join([]string{
+		"        ",
+		"        ",
+		"        ",
+		"        ",
+		"        ",
+		"        ",
+		"        ",
+		"        ",
+	}, "\n"))
+}
+
+func TestLocationDecoding(t *testing.T) {
+	location, err := fileRankFromString("a1")
+	assert.Nil(t, err)
+	assert.Equal(t, location, FileRank{0, 0})
+
+	game, err := GamestateFromFenString("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
+	assert.Nil(t, err)
+
+	assert.Equal(t, pieceAtFileRank(game.Board, location).String(), WR.String())
+
+	location, err = fileRankFromString("e4")
+	assert.Nil(t, err)
+	assert.Equal(t, location, FileRank{4, 3})
+
+	assert.Equal(t, pieceAtFileRank(game.Board, location).String(), WP.String())
+
+	location, err = fileRankFromString("d8")
+	assert.Nil(t, err)
+	assert.Equal(t, location, FileRank{3, 7})
+
+	assert.Equal(t, pieceAtFileRank(game.Board, location).String(), BQ.String())
+
+	location, err = fileRankFromString("a1")
+	assert.Nil(t, err)
+	assert.Equal(t, boardIndexFromFileRank(location), 0)
+
+	location, err = fileRankFromString("h1")
+	assert.Nil(t, err)
+	assert.Equal(t, boardIndexFromFileRank(location), 7)
+
+	location, err = fileRankFromString("a8")
+	assert.Nil(t, err)
+	assert.Equal(t, boardIndexFromFileRank(location), 56)
+
+	location, err = fileRankFromString("h8")
+	assert.Nil(t, err)
+	assert.Equal(t, boardIndexFromFileRank(location), 63)
+}
+
+func TestNotationDecoding(t *testing.T) {
+	s := "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
+	g, err := GamestateFromFenString(s)
+	assert.Nil(t, err)
+
+	assert.Equal(t, g.Board.String(), NaturalBoardArray{
+		BR, BN, BB, BQ, BK, BB, BN, BR,
+		BP, BP, BP, BP, BP, BP, BP, BP,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, WP, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		WP, WP, WP, WP, XX, WP, WP, WP,
+		WR, WN, WB, WQ, WK, WB, WN, WR,
+	}.AsBoardArray().String())
+
+	assert.Equal(t, g.player, BLACK)
+
+	expectedLocation, err := fileRankFromString("e3")
+	assert.Nil(t, err)
+	assert.Equal(t, g.enPassantTarget.Value(), expectedLocation)
+
+	assert.Equal(t, g.whiteCanCastleKingside(), true)
+	assert.Equal(t, g.whiteCanCastleQueenside(), true)
+	assert.Equal(t, g.blackCanCastleKingside(), true)
+	assert.Equal(t, g.blackCanCastleQueenside(), true)
+
+	assert.Equal(t, g.halfMoveClock, 0)
+	assert.Equal(t, g.fullMoveClock, 1)
+}
+
+func TestNotationDecoding2(t *testing.T) {
+	s := "8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 w - - 99 50"
+	g, err := GamestateFromFenString(s)
+	assert.Nil(t, err)
+
+	assert.Equal(t, g.Board, NaturalBoardArray{
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, BK, XX, XX,
+		XX, XX, XX, BP, XX, XX, XX, XX,
+		XX, BP, XX, WP, BP, XX, XX, BP,
+		BP, WP, XX, XX, WP, BP, XX, WP,
+		WP, XX, XX, XX, XX, WP, XX, WK,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+		XX, XX, XX, XX, XX, XX, XX, XX,
+	}.AsBoardArray())
+	assert.Equal(t, g.player, WHITE)
+
+	assert.Equal(t, true, g.enPassantTarget.IsEmpty())
+
+	assert.Equal(t, g.whiteCanCastleKingside(), false)
+	assert.Equal(t, g.whiteCanCastleQueenside(), false)
+	assert.Equal(t, g.blackCanCastleKingside(), false)
+	assert.Equal(t, g.blackCanCastleQueenside(), false)
+
+	assert.Equal(t, g.halfMoveClock, 99)
+	assert.Equal(t, g.fullMoveClock, 50)
+}
+
+func TestUCI(t *testing.T) {
+	inputs := []string{
+		"isready",
+		"uci",
+		"position fen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+		"go",
+	}
+	r := Runner{}
+	for _, line := range inputs {
+		r.HandleInputAndReturnDone(line)
+	}
+}
+
 func TestSingleBoards(t *testing.T) {
 	assert.Equal(t, singleBitboard(63).String(), strings.Join([]string{
 		"00000001",
